27f44fc5c344b2b51b72c1124c7dcdfae8aae3fd
ajospideu1-spec <ajospideu1@gmail.com>
2025-10-16 23:13:10 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import NotFound from "./pages/NotFound";
   const removeFloating = () => {
 
   document.querySelectorAll('[style*="position: fixed"][style*="bottom: 1rem"][style*="right: 1rem"][style*="z-index: 2147483647"]').forEach(el => el.remove());
-
 };
 
 
