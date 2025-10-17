043cfe8bbc3aa49858fad478b580efddea607cf2
ajospideu1-spec <ajospideu1@gmail.com>
2025-10-16 23:12:49 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
+  const removeFloating = () => {
+
+  document.querySelectorAll('[style*="position: fixed"][style*="bottom: 1rem"][style*="right: 1rem"][style*="z-index: 2147483647"]').forEach(el => el.remove());
+
+};
+
+
 const queryClient = new QueryClient();
 
 const App = () => (
